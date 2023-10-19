816ed873c0127a3307d7ef4ef1c79f94fbc3a34b
Yulduz.Saidova <yulduz.saidova@MacBook-Air-YulduzSaidova.local>
2023-10-19 13:30:02 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+// 1 mashq
+
+
+let arr = ['Ahmad', 'Behruz', 'Aziz', 'Amin'  ]
+function Fn(...name){
+    for(let i = 0; i<=arr.length ; i++){
+        if(name == arr[i]){
+            console.log(`arr ichida ${name} bor`);
+           }else{
+
+           }
+    }
+}
+Fn('Behruz')
+
+
+// 2 mashq
+
+
+// let arr1 = [8, 17, 5, 7, 34, 3, 6, 25]
+// console.log(arr1);
+// arr1.sort(function (a, b) { return a - b })
+// console.log(`arr1 ichidagi eng kichik son ${arr1[0]}`);
+// console.log(`arr1 ichidagi eng katta son ${arr1.at(-1)}`);
+
+
+// 3 mashq
+
+// let arr1 = [2, 6, 8, -4, 9, 21, -13, -1, 28]
+// let arr2 = []
+// for(let i = 0; i <= arr1.length ; i++){
+//     if(arr1[i] > 0){
+//      arr2.push(arr1[i])
+//     }
+// }
+// console.log(arr2);
